fec4b25613de128d4d8dd5b841a671ba7e1c0ce5
Rishabh <rishabh@Rishabhs-MacBook-Air.local>
2026-05-24 05:31:08 +0530

    image generation backend completed

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from remindee-backend!")
+import logging
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from database import create_tables
+from routes import router
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_tables()
+    yield
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(
+    title="Remindee Backend",
+    description="Backend service for Remindee app, have multiple AI services",
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000", "https://remindee.ai"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
